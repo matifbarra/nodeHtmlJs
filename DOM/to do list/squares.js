5d2994f9a69e370c8a3605f00adf9b031f8cc2d9
Arnoldo Alvarez <arnoldo.alvarez75@yahoo.com>
2020-09-03 09:30:15 -0400

    actualizando archivosarchivos, nueva aplicacion de todoList con divs

--- DOM/to do list/squares.js
+++ DOM/to do list/squares.js
@@ -1,40 +1,45 @@
-const arrayS = JSON.parse(localStorage.getItem('storage'))  || [];
 const arrayT = [];
+const arrayS = JSON.parse(localStorage.getItem('storage'))  || [];
 
 
-// const arrayT = JSON.parse(localStorage.getItem('tittles'))  || [];
+
  
-const render = () =>{
-    let h;
-    const box = document.getElementById('squaresBox');
-    const squaresMap = arrayS.map(t =>
-    '<div class="squares">' + '<h2 class="tittles">' 
-    + 'Titulo' + '</h2>' 
-    + t + '</div>');
-    console.log(arrayT);
+    const render = () =>{
     
-    
-    
-    box.innerHTML = squaresMap.join('');
-    const elemento = document.querySelectorAll('#squaresBox div');
+        let h;
+        const box = document.getElementById('squaresBox');
+        const squaresMap = arrayS.map(t =>
+        '<div class="squares">' + '<h2 class="tittles">' 
+        + 'Titulo' + '</h2>' 
+        + t + '</div>');
+        console.log(arrayT);
+        box.innerHTML = squaresMap.join('');
+        const elemento = document.querySelectorAll('#squaresBox div');
+
+
     elemento.forEach((elemento,i)=>{
         console.log('i: ' + i);
         elemento.addEventListener('click',() =>{
-            elemento.parentNode.removeChild(elemento);
-            arrayS.splice(i,1); // Eliminando datos del arreglo con Array.splice
-            upDate(arrayS); //Actualizando en 'localStorage'
-            render();
-            console.log(arrayS, 'i: ' + i);    
+        elemento.parentNode.removeChild(elemento);
+        arrayS.splice(i,1); // Eliminando datos del arreglo con Array.splice
+        arrayT.splice(i,1);
+        upDate(arrayS); //Actualizando en 'localStorage'
+        render();
+        console.log(arrayS, 'i: ' + i);    
         })
-    })
+    });
+    
+    
+  
 }
+
 
 // Para guardar elementos en el 'localStorage'
 const upDate = (arrayS) => {
     const arraySstring = JSON.stringify(arrayS);
-    // const arrayTstring = JSON.stringify(arrayT);
+    const arrayTstring = JSON.stringify(arrayT);
     localStorage.setItem('storage', arraySstring);
-    // localStorage.setItem('tittles', arrayT);
+    localStorage.setItem('storage2', arrayTstring);
     }
 
 
@@ -44,15 +49,15 @@ window.onload = () => {
     form.onsubmit = (e) => {
         e.preventDefault();
         const squaresTittle = document.getElementById('squaresTittle');
-        const texto2 = squaresTittle.value;
+        const texto = squaresTittle.value;
         const squaresText = document.getElementById('squaresText');
-        const texto = squaresText.value;
-        console.log(texto2);
+        const texto2 = squaresText.value;
         console.log(texto);
+        console.log(texto2);
         squaresTittle.value = ' ';
         squaresText.value = ' ';
-        arrayT.push(texto2);
-        arrayS.push(texto);
+        arrayT.push(texto);
+        arrayS.push(texto2);
         upDate(arrayS);
         render();
     }
