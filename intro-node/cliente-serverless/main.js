db70d719199802058957c9690a50bf0ab04f0189
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2020-09-26 10:00:10 -0400

    Modificaciones en cliente-serverless,cliente-test

--- intro-node/cliente-serverless/main.js
+++ intro-node/cliente-serverless/main.js
@@ -1,11 +1,36 @@
+
+// metodo regular****************************************************************
+// window.onload = () => {
+//     fetch('http://localhost:3000/api/meals')
+//     .then(response => response.json())
+//     .then(data => {
+//         const mealsList = document.getElementById('meals-list');
+//         const btn = document.getElementById('btn');
+//         console.log(btn);
+//         const mealsMap = data.map(t => '<li>' + t.name + '</li>').join('');
+//         mealsList.innerHTML = mealsMap;
+//         btn.removeAttribute('disabled');
+
+//     })
+// }
+// ********************************************************************************
+
+// With templates String 
 window.onload = () => {
+    const renderItem = (t) =>{
+        return `<li data-id="${t._id}">${t.name}</li>`
+
+    }
+
     fetch('http://localhost:3000/api/meals')
     .then(response => response.json())
     .then(data => {
         const mealsList = document.getElementById('meals-list');
-        const mealsMap = data.map(t => '<li>' + t.name + '</li>').join('');
+        const btn = document.getElementById('btn');
+        console.log(btn);
+        const mealsMap = data.map(renderItem).join('');
         mealsList.innerHTML = mealsMap;
+        btn.removeAttribute('disabled');
 
     })
-
 }
--- intro-node/cliente-test/main.js
+++ intro-node/cliente-test/main.js
@@ -2,10 +2,53 @@ window.onload = () => {
     fetch('http://localhost:3000/api/meals')
     .then(response => response.json())
     .then(data => {
-        const mealsList = document.getElementById('meals-list');
-        const mealsMap = data.map(t => '<li>' + t.name + '</li>').join('');
-        mealsList.innerHTML = mealsMap;
+        // const mealsList = document.getElementById('meals-list');
+        // const mealsMap = data.map(t => '<li id = "meal">' + t.name + '</li>').join('');
+        // mealsList.innerHTML = mealsMap;
+        const elemento = document.querySelectorAll('#meal');
+        elemento.forEach((elemento,i)=>{
+        console.log(elemento);
+        console.log('i: ' + i);
 
+        elemento.addEventListener('click',() =>{
+            // id = '5f6cafd251be5d131ca848c5';
+            // console.log('Ay me pegaron');
+            // elemento.parentNode.removeChild(elemento);
+            // fetch(`http://localhost:3000/api/meals/${id}`, { 
+            //     method:'DELETE' 
+            // })
+            // .then(res => res.json())
+            // .then(res => {
+            //     if (res.success){
+            //         console.log('Eliminado')
+            //     }
+            // })
+
+            const info = {
+                name: 'Arnoldo',
+                desc: "el mejor"
+            }
+
+            fetch("https://jsonplaceholder.typicode.com/posts", { 
+      
+                // Adding method type 
+                method: "POST",
+                // Adding headers to the request 
+                headers: { 
+                    "Content-type": "application/json; charset=UTF-8"
+                }, 
+                  
+                // Adding body or contents to send 
+                body: JSON.stringify({ info }), 
+            }) 
+              
+            // Converting to JSON 
+            .then(response => response.json()) 
+              
+            // Displaying results to console 
+            .then(response => console.log(response));
+        })
     })
-
+})
 }
+ 
